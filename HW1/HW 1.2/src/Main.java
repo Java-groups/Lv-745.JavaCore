59d0468ce5c5e849545413f27199aec42df975a2
AlanaSagaidak <109764549+AlanaSagaidak@users.noreply.github.com>
2023-01-20 23:24:42 +0200

    hw 1.2

---
+++ HW1/HW 1.2/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("What is your name?");
+		String name = br.readLine();
+		System.out.println("Where are you live, " + name + "?");
+		String address = br.readLine();
+		System.out.println("Hello, "+ name + "You live in "+ address);
+
+	}
+
+}
