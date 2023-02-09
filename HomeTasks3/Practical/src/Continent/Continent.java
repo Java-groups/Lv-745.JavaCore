180c3a112faadead4989c49f0d79216943b99792
vasyldmitrovich <48633322+vasyldmitrovich@users.noreply.github.com>
2023-02-09 15:07:10 +0200

    Update Continent.java
    I Added comments, please fix

--- HomeTasks3/Practical/src/Continent/Continent.java
+++ HomeTasks3/Practical/src/Continent/Continent.java
@@ -3,7 +3,18 @@ package Continent;
 public enum Continent {
     Asia, Africa, Europe, NorthAmerica, SouthAmerica, Australia, Antarctica
 }
+/*This code is not full
+must be something like that
+AFRICA("Algeria"
+            , "Angola"
+            , "Benin"
+            , "Botswana"
+            , "Burkina Faso"
+            , "Uganda"
+            , "Zambia"
+            , "Zimbabwe"),
 
+    ASIA("Afghanistan" etc...*/
 
 
 
