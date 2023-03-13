d0678c175297d9d31a3717f517ecc838797440e9
qparmacevt <53297882+qparmacevt@users.noreply.github.com>
2023-03-13 19:34:51 +0200

    Task12 Task14 Added

---
+++ src/Task14.java
@@ -0,0 +1,16 @@
+public class Task14 {
+    public static int countCoinCombinations() {
+        int[] coins = {1, 2, 5, 10, 20, 50, 100, 200};
+        int[] ways = new int[201];
+        ways[0] = 1; // there's only one way to make 0p
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= 200; j++) {
+                ways[j] += ways[j - coins[i]];
+            }
+        }
+
+        return ways[200];
+    }
+
+}
